81229d7ec8e9181caff6091ed2ced6b362d9d4d3
Franz Labrador <19819211@student.curtin.edu.au>
2023-09-01 13:24:03 +0800

    Test List of institutions

---
+++ crl_database/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { TableList } from './components/TableList/TableList';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>
+        Institution List
+      </h1>
+      <TableList />
+    </div>
+  );
+}
+
+export default App;
