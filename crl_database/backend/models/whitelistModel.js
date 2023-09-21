fae8542411cb78d167736bc0cfe2ad04946198c7
michaelPella <20156093@student.curtin.edu.au>
2023-09-21 17:46:41 +0800

    latest version commit

--- crl_database/backend/models/whitelistModel.js
+++ crl_database/backend/models/whitelistModel.js
@@ -10,13 +10,13 @@ const whitelistedUsersSchema = new Schema({
         type: Number,
         required: true,
         unique: true,
-        validate: {
+        /*validate: {
             validator: function(v) {
                 // Convert the number to string and check if it's numeric and of length 8
                 return validator.isNumeric(String(v)) && String(v).length === 8;
             },
             message: 'Invalid Curtin ID for user'
-        }
+        }*/
     },
     role: {
         type: String,
@@ -26,3 +26,22 @@ const whitelistedUsersSchema = new Schema({
 
 
 export default mongoose.model('WhitelstdUser', whitelistedUsersSchema)
+
+
+/* validate: {
+    validator: function(v) {
+       
+        return /^[0-9]{6}.$/.test(String(v));
+    },
+    message: props => `${props.value} is not a valid format. Expected: 6 digits followed by any character.`
+    
+    Front end
+
+    function isValidCurtinID(value) {
+    return /^[0-9]{6}.$/.test(value);
+}
+
+
+if (!isValidCurtinID(curtinID)) {
+    console.error("Invalid Curtin ID format");
+} */
--- crl_database/backend/utils/sendVerificationEmail.js
+++ crl_database/backend/utils/sendVerificationEmail.js
@@ -8,8 +8,8 @@ export const sendVerificationEmail = (user) => {
     from: '"CRL-Database-App" <crl-database@outlook.com>',
     to: user.email,
     subject: " Verify your email to gain access to the CRL Student Database",
-    html: `<p> Hello ${user.username} @${user.email} 👋, ☑ verify your email by clicking this link...</p>
-        <a href = "${process.env.CLIENT_URL}/verifyemail?emailToken=${user.emailToken}">Verify Your Email</a>`,
+    html: `<p> Hello ${user.username} @${user.email} 👋, verify your email by clicking this link...</p>
+        <a href = "${process.env.CLIENT_URL}/verifyemail?emailToken=${user.emailToken}">☑Verify Your Email</a>`,
 }
 
 
