7ace0d48260b9fd9072d913a5e65099f03474bca
michaelPella <20156093@student.curtin.edu.au>
2023-10-14 13:55:30 +0800

    changees for front-end student search -- franz

--- crl_database/frontend/src/components/studentSearch.jsx
+++ crl_database/frontend/src/components/studentSearch.jsx
@@ -11,13 +11,13 @@ const StudentSearch = ({ open, onClose}) => {
     const [searchInput, setSearchInput] = useState('')
     const [searchResults, setSearchResults] = useState([])
     const [suggestedResults, setSuggestedResults] = useState([])
-    const [showExtraInfo, setShowExtraInfo] = useState(false);
-    const [selectedCase, setSelectedCase] = useState(null);
+   
 
     const styles = useStyles()
 
     const {user} = useAuthContext()
 
+    // Make a smooth delay when selecting an item from the search
     useEffect(() => {
       const delayDebounceFn = setTimeout(() => {
         if (searchInput) {
@@ -29,8 +29,7 @@ const StudentSearch = ({ open, onClose}) => {
       return () => clearTimeout(delayDebounceFn);
      }, [searchInput]);
 
-     const handleListItemClick = (student) => {
-      setSelectedCase(student.studentNotes);
+     const handleClick = (student) => {
       setSuggestedResults([]);
       setSelectedCase(null); // ensure no case is selected when displaying notes
     }
@@ -42,7 +41,7 @@ const StudentSearch = ({ open, onClose}) => {
         //Update the state with the results
 
         try {
-          const response = await fetch(`http://localhost:5001/applications/studentSearch?q=${searchInput}`, {
+          const response = await fetch(`http://localhost:5001/applications/searchInput?q=${searchInput}`, {
             headers: {
                 'Authorization': `Bearer ${user.token}`
             }
@@ -61,22 +60,6 @@ const StudentSearch = ({ open, onClose}) => {
       }
     }
 
-
-    const extractNote = (note) => {
-
-        if (!note) return { id: null, note: null }
-
-        const idMatch = note.match(/(\d{8})/)
-  
-
-        return {
-          id: idMatch ? idMatch[1] : null,
-          note: note
-      }
-  }
-
-
-
     return (
       <>
       <Modal
@@ -88,103 +71,42 @@ const StudentSearch = ({ open, onClose}) => {
           maxHeight="xs"
           fullWidth
       >
-          
-              <div className={styles.modalContainer}>
-              <div className={styles.closeButton} onClick={() => setShowExtraInfo(true)}>
-              Extra Info
-              </div>
-                
+        
+        
+        <TextField 
+            
+            onChange={(e) => setSearchInput(e.target.value)} 
+            placeholder="Search by ID or Name" 
+        />
 
-              <TextField 
-                  value={searchInput} 
-                  onChange={(e) => setSearchInput(e.target.value)} 
-                  placeholder="Search by ID or Name" 
-              />
+        <Link to="/applications"> 
+          <Button variant="contained" onClick={handleSearch} color="primary" style={{ marginLeft: '10px' }}>
+              Search
+          </Button>
+      </Link>
 
-              <div className={styles.resultList}>
-                  {suggestedResults.map(student => (
-                      <ListItem 
-                          key={student._id} 
-                          button 
-                          onClick={() => {
-                              setSearchInput('');
-                              setSelectedCase(student);
-                              setSuggestedResults([]); 
+        <div className={styles.resultList}>
+            {suggestedResults.map(student => (
+            <ListItem 
+                key={student._id} 
+                button 
+                onClick={() => {
+                    setSearchInput('');
+                    setSelectedCase(student);
+                    setSuggestedResults([]); 
                           }}
-                      >
-                           <Typography variant="body1" className={styles.noteText}>
-                          {student.studentNotes}
-                          </Typography>
-                      </ListItem>
+             >
+            <Typography variant="body1" className={styles.noteText}>
+                {student.studentNotes}
+            </Typography>
+            </ListItem>
                   ))}
-              </div>
+        </div>
 
-              {/* Displaying selected student's notes */}
-              {selectedCase && (
-              <div style={{ marginTop: '20px' }}>
-                <Typography variant="h4" className={styles.noteText}>
-                  {selectedCase.studentNotes}
-                </Typography>
-              </div>
-             )}
-
-              <List>
-                  {searchResults.map(student => {
-                      if (student && student.studentNotes) {
-                          const { id, note } = extractNote(student.studentNotes);
-                          return (
-                              <ListItem key={student._id} className={styles.listItem}>
-                                  <div>
-                                      {id && <Typography variant="body1" className={styles.idText}>ID: {id}</Typography>}
-                                      <Typography variant="body1" className={styles.noteText}>Note: {note}</Typography>
-                                  </div>
-                              </ListItem>
-                          );
-                      }
-                      return null;
-                  })}
-              </List>
-          </div>
+             
       </Modal>
 
-      <Modal
-          open={showExtraInfo}
-          onClose={() => setShowExtraInfo(false)}
-          aria-labelledby="selected-case-modal"
-          aria-describedby="details-of-selected-case"
-      >
-          <div style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              width: '40%',
-              height: 'auto',
-              backgroundColor: 'white',
-              padding: '20px',
-              borderRadius: '8px',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-          }}>
-              {selectedCase && (
-                  <>
-                      <Typography variant="h2" style={{ color: 'black', marginBottom: '15px' }}>
-                          Assessed Units:
-                          <ul>
-                              {selectedCase.assessedUnits.map((unit, index) => (
-                                  <li key={index}>{unit.name}</li>
-                              ))}
-                          </ul>
-                      </Typography>
-                      <Typography variant="h2" style={{ color: 'black', marginBottom: '15px' }}>
-                          Assessor: {selectedCase.assessor}
-                      </Typography>
-          
-                  </>
-              )}
-          </div>
-      </Modal>
+     
   </>
 )
 }
